0e06f1555672022d0d8a7cc4d41a6c74c9fb9909
ylizzr <ylizarazor@ucentral.edu.co>
2025-05-27 19:35:24 -0500

    Ajuste htpps

--- src/main/java/com/EnergiaIngeligente/microfacturacion/configuracion/WebConfig.java
+++ src/main/java/com/EnergiaIngeligente/microfacturacion/configuracion/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://energiainteligente.ddns.net:8082")
+                        .allowedOrigins("https://energiainteligente.ddns.net")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("Set-Cookie")
